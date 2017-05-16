7727ac887ac56dad1d032a02768c24d4b930f8a0
Anstasiya228 <staska_1997@mail.ru>
2017-05-17 00:52:32 +0300

    User and Role model (for spring security

--- src/main/java/ua/goit/startupserviceapp/model/Role.java
+++ src/main/java/ua/goit/startupserviceapp/model/Role.java
@@ -13,6 +13,17 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     //TODO need to add other fields
 
 }
--- src/main/java/ua/goit/startupserviceapp/model/User.java
+++ src/main/java/ua/goit/startupserviceapp/model/User.java
@@ -3,15 +3,70 @@ package ua.goit.startupserviceapp.model;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-@Table (name = "users")
+@Table(name = "users")
 public class User {
 
     @Id
     @Column(unique = true, nullable = false)
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @Transient
+    transient private String confirmPassword;
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns=@JoinColumn(name="user_id"),
+    inverseJoinColumns=@JoinColumn(name="role_id"))
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     //TODO need to add other fields
 }
