af0ee6e0b546f6bed85df9f07e8edb80fcdad713
Illia Kulinich <ilya.v.kulinich@gmail.com>
2017-05-17 18:12:24 +0300

    1. refactored pom file 2. added to pom servlet and mockito dependecies 3. created UserService and ProductService interfaces

---
+++ src/main/java/ua/goit/startupserviceapp/service/ProjectServiceImpl.java
@@ -0,0 +1,79 @@
+package ua.goit.startupserviceapp.service;
+
+import ua.goit.startupserviceapp.model.Project;
+
+import java.util.List;
+
+/**
+ * Implementation of {@link ua.goit.startupserviceapp.service.UserService}
+ *
+ * @author Illia Kulinich
+ * @version 1.0
+ */
+
+public class ProjectServiceImpl implements ProjectService {
+    @Override
+    public void save(Project project) {
+
+    }
+
+    @Override
+    public void edit(Project project) {
+
+    }
+
+    @Override
+    public void delete(Project project) {
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+
+    }
+
+    @Override
+    public void ready(Project project) {
+
+    }
+
+    @Override
+    public void approve(Project project) {
+
+    }
+
+    @Override
+    public void reject(Project project) {
+
+    }
+
+    @Override
+    public List<Project> getAllProjects() {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjectsByFounder() {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjectsByName() {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjectsByCategory() {
+        return null;
+    }
+
+    @Override
+    public List<Project> getProjectsByStatus() {
+        return null;
+    }
+
+    @Override
+    public Project getProjectById(int id) {
+        return null;
+    }
+}
