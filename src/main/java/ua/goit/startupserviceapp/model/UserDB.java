c31c0ed2e5eec3b16d4a8b39d3fb23331fad5fd7
Galina <gala6ap@mail.ru>
2017-05-18 23:23:12 +0300

    Classes UserDB and ProjectEvaluation was added.

---
+++ src/main/java/ua/goit/startupserviceapp/model/UserDB.java
@@ -0,0 +1,150 @@
+package ua.goit.startupserviceapp.model;
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class UserDB {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String country;
+    private String city;
+    private String login;
+    private String password;
+    private int active;
+    private Set<ProjectEvaluation> marks = new HashSet<>();
+
+    public UserDB() {
+    }
+
+    public UserDB(String firstName, String lastName, String email, String login, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.login = login;
+        this.password = password;
+    }
+
+    @Id
+    @Column(name = "user_id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", nullable = false)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "phone_number")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "country")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Column(name = "city")
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Column(name = "login", nullable = false)
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "is_active")
+    public int isActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @OneToMany(mappedBy = "user")
+    public Set<ProjectEvaluation> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(Set<ProjectEvaluation> marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDB{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", city='" + city + '\'' +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
