9eef6c7ed4bd9c4b3564e5d735a924e613fc4a62
Gluck-mz <aleksey.orishchenko@gmail.com>
2017-05-30 02:39:23 +0200

    Added ategoryRepository class

---
+++ src/main/java/ua/goit/startupserviceapp/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package ua.goit.startupserviceapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.goit.startupserviceapp.model.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
