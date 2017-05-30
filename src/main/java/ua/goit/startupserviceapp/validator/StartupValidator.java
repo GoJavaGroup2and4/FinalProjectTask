17e352539a92b9e5a59ab1835d5dfbb753496101
Gluck-mz <aleksey.orishchenko@gmail.com>
2017-05-30 02:40:46 +0200

    added StartupValidator class

--- src/main/java/ua/goit/startupserviceapp/validator/StartupValidator.java
+++ src/main/java/ua/goit/startupserviceapp/validator/StartupValidator.java
@@ -1,12 +1,14 @@
 package ua.goit.startupserviceapp.validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 import ua.goit.startupserviceapp.model.Startup;
 import ua.goit.startupserviceapp.service.StartupService;
 
+@Component
 public class StartupValidator implements Validator{
 
     @Autowired
